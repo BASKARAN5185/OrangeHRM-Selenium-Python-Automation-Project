bf9851978a35cd5a4999d5d274358db450641e40
BASKARAN5185 <baskarbala5185@gmail.com>
2025-06-19 16:57:47 +0530

    Login Page New Testcased Added

--- pages/Login_Page.py
+++ pages/Login_Page.py
@@ -1,19 +1,23 @@
+from xml.dom.minidom import Element
 from selenium.webdriver.common.by import By
 
 class Login_Page:
     def __init__(self,driver) :
         self.driver=driver
-        self.username=(By.ID,"login")
-        self.password=(By.ID,"password")
-        self.loginbutton=(By.XPATH,"//*[@id='wrapwrap']/main/div/div/div/div/div/form/div[3]/button")
+        self.username=(By.NAME,"username")
+        self.password=(By.NAME,"password")
+        self.loginbutton=(By.XPATH,"//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")
         
     def enter_username(self,username):
-        self.driver.find_element(*self.username).send_keys(username)
-        
+        user1=self.driver.find_element(*self.username)
+        user1.clear()
+        user1.send_keys(username)
         
     def enter_password(self,password):
-        self.driver.find_element(*self.password).send_keys(password) 
-        
+        password1=self.driver.find_element(*self.password)
+        password1.clear()
+        password1.send_keys(password)   
+    
     def click_loginbutton(self):
         self.driver.find_element(*self.loginbutton).click()
         
--- test/Login_Page_Test.py
+++ test/Login_Page_Test.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from typing import Self
 import unittest
 from selenium import webdriver
 
@@ -19,14 +20,40 @@ class Login_Page_Test(unittest.TestCase):
         cls.driver.quit()
 
     def setUp(self):
-        self.driver.get("https://stg.rms.mo.vc/web/login")
+        self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
         self.login_page = Login_Page(self.driver)
 
-    def test_valid_login(self):
-        self.login_page.enter_username("testingteam@gmail.com")
-        self.login_page.enter_password("Solution@456")
+    def test_Invalid_login(self):
+        self.login_page.enter_username("testiteam@gmail.com")
+        self.login_page.enter_password("Solon@456")
         self.login_page.click_loginbutton()
-        self.assertIn("Dashboard", self.driver.title)
-
+       # self.assertIn("Dashboard", self.driver.title)
+    
+    def test_Invalid_username(self):
+        self.login_page.enter_password("Admi_n")
+        self.login_page.enter_password("admin123")   
+        self.login_page.click_loginbutton()
+        print(self.driver.title)
+        
+    def test_Invalid_Password(self):
+        self.login_page.enter_username("Admin")
+        self.login_page.enter_password("jhjhgj")
+        self.login_page.click_loginbutton()
+        
+    def test_empty_username(self):
+        self.login_page.enter_username("")
+        self.login_page.enter_password("admin123")  
+        self.login_page.click_loginbutton() 
+        
+    def test_empty_Password(self):
+        self.login_page.enter_username("Admin")
+        self.login_page.enter_password("")  
+        self.login_page.click_loginbutton()
+     
+    def test_empty_usernameandpassword(self):
+        self.login_page.enter_username("")
+        self.login_page.enter_password("")  
+        self.login_page.click_loginbutton()            
+        
 if __name__ == "__main__":
     unittest.main()
